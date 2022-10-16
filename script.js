67028d8f38b3eac8c84697dbc94dfca390c2f791
Maurice Roach <mroach1993@hotmail.com>
2022-10-16 18:37:09 -0400

    added highscore function and color changes

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const guess = document.querySelector(".guess");
 const check = document.querySelector(".check");
 const again = document.querySelector(".again");
 const score = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
 
 let random = Math.trunc(Math.random() * 10) + 1;
 let lastScore = 10;
-
-// add statement that if lastScore = 0, diplay message you lost
+let hScore = 0;
 
 check.addEventListener("click", function () {
   if (guess.value == random) {
@@ -18,24 +18,50 @@ check.addEventListener("click", function () {
     number.textContent = guess.value;
     number.style.backgroundColor = "#70e000";
     score.textContent = lastScore;
+    if (lastScore > hScore) {
+      hScore = lastScore;
+      highscore.textContent = hScore;
+    }
   } else if (guess.value > random) {
     lastScore--;
     score.textContent = lastScore;
     msg.textContent = "Too High";
     msg.style.color = "";
-    msg.style.color = "red";
-    number.style.backgroundColor = "red";
+    msg.style.color = "orange";
+    number.style.backgroundColor = "orange";
+    if (lastScore === 0) {
+      msg.textContent = "Game Over";
+      msg.style.color = "red";
+      number.style.backgroundColor = "red";
+    }
   } else if (guess.value < random) {
     lastScore--;
     score.textContent = lastScore;
     msg.textContent = "Too Low";
     msg.style.color = "";
-    msg.style.color = "red";
-    number.style.backgroundColor = "red";
+    msg.style.color = "orange";
+    number.style.backgroundColor = "orange";
+    if (lastScore === 0) {
+      msg.textContent = "Game Over";
+      msg.style.color = "red";
+      number.style.backgroundColor = "red";
+    }
   }
 });
 
 again.addEventListener("click", function () {
+  msg.textContent = "START GUESSING";
+  guess.value = "";
+  number.textContent = "?";
+  number.style.backgroundColor = "#eee";
+  score.textContent = "10";
+  msg.style.color = "";
+  random = Math.trunc(Math.random() * 10) + 1;
+});
+
+// reset the game
+again.addEventListener("click", function () {
+  lastScore = 10;
   msg.textContent = "START GUESSING";
   guess.value = "";
   number.textContent = "?";
